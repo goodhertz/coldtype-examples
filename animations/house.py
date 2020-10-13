7fedea5af4bb70f8bb1e63051786174d42848d12
Rob Stenson <rob.stenson@gmail.com>
2020-10-13 12:00:17 -0700

    minor edit on house

--- animations/house.py
+++ animations/house.py
@@ -3,12 +3,13 @@ from coldtype.animation.midi import *
 
 
 obvs = Font("fonts/ColdtypeObviously.designspace")
-drums = MidiReader(Path("animations/media/house.mid").resolve(), duration=60, bpm=120)[0]
+midi = Path("animations/media/house.mid").resolve()
+drums = MidiReader(midi, duration=60, bpm=120)[0]
 
 
-@animation(duration=drums.duration, storyboard=[56], bg=0.1)
+@animation(duration=drums.duration, storyboard=[0], bg=0.1)
 def render(f):
-    kick = drums.fv(f.i, [36], [12, 12]).ease()
+    kick = drums.fv(f.i, [36], [12, 10]).ease()
     snare = drums.fv(f.i, [38], [4, 35]).ease()
     hat_count = drums.fv(f.i, [42], [0, 1]).count
     cowbell = drums.fv(f.i, [48], [0, 50]).ease()
@@ -18,13 +19,13 @@ def render(f):
     type_pens = StyledString("TYPE", style.mod(tu=-150-100*kick, rotate=-8*kick)).pens()
 
     chord_change = hat_count < 3
-    cold_pens.f(0.6j if chord_change else 0.75j, 0.75, 0.5)
-    type_pens.f(0.05j if chord_change else 0.9j, 0.75, 0.5)
+    cold_pens.f(hsl(0.6 if chord_change else 0.75, 0.75, 0.5))
+    type_pens.f(hsl(0.05 if chord_change else 0.9, 0.75, 0.5))
     
     for o in cold_pens.glyphs_named("O"):
-        o.mod_contour(1, lambda c: c.rotate(hat_count*45))
+        o.mod_contour(1, lambda c: c.rotate(hat_count*-45))
     for p in type_pens.glyphs_named("P"):
-        p.mod_contour(1, lambda c: c.translate(50*cowbell, 0))
+        p.mod_contour(1, lambda c: c.translate(70*cowbell, 0))
 
     r = f.a.r.inset(0, 150)
     return DATPenSet([
