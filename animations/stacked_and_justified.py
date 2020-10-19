7399ab000753cd8fe82d8c5c990994c23427d375
Rob Stenson <rob.stenson@gmail.com>
2020-10-19 13:47:13 -0700

    slight styling update

--- animations/stacked_and_justified.py
+++ animations/stacked_and_justified.py
@@ -6,10 +6,13 @@ fatface = Font("~/Type/fonts/fonts/OhnoFatfaceVariable.ttf")
 @animation(timeline=Timeline(100, storyboard=[0]), bg=0.2)
 def render(f):
     at = f.a.progress(f.i, loops=1, easefn="eeio")
-    c1, c2 = [r.inset(20, 5) for r in f.a.r.inset(0, 50).divide(0.15+at.e*0.7, "maxy")]
+    cs = f.a.r.inset(0, 50).divide(0.15+at.e*0.7, "maxy")
+    c1, c2 = [r.inset(20, 5) for r in cs]
     s = Style(fatface, t=-25, wdth=1, wght=1, ro=1, r=1)
-    stacked_and = StyledString("STACKED &", s.mod(fitHeight=c1.h, opsz=at.e)).fit(c1).pens()
-    justified = StyledString("JUSTIFIED", s.mod(fitHeight=c2.h, opsz=1-at.e)).fit(c2).pens()
+    stacked_and = StyledString("STACKED &",
+        s.mod(fitHeight=c1.h, opsz=at.e)).fit(c1).pens()
+    justified = StyledString("JUSTIFIED",
+        s.mod(fitHeight=c2.h, opsz=1-at.e)).fit(c2).pens()
 
     return DATPenSet([
         stacked_and.align(c1).trackToRect(c1, pullToEdges=1, r=1).f(0, 1, 0),
